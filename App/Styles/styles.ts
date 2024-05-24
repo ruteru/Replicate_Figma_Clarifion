f53388b54b63a525cf238cc9dc2d27431c171439
ruteru <garcicon45@gmail.com>
2024-05-24 17:28:39 -0300

    apply styles

--- App/Styles/styles.ts
+++ App/Styles/styles.ts
@@ -3,9 +3,9 @@ import { StyleSheet, Platform } from 'react-native';
 export const logo_styles = StyleSheet.create({
     logoContainer: {
         position: 'absolute',
-        top: Platform.OS === 'web' ? 80 : 75,
-        left: Platform.OS === 'web' ? 15 : 10,
-        width: Platform.OS === 'web' ? '18%' : '50%',
+        top: Platform.OS === 'web' ? 50 : 75,
+        left: Platform.OS === 'web' ? 45 : 10,
+        width: Platform.OS === 'web' ? '18%' : '40%',
         height: Platform.OS === 'web' ? '18%' : '10%',
         justifyContent: 'center',
         alignItems: 'center',
@@ -13,32 +13,74 @@ export const logo_styles = StyleSheet.create({
     logo: {
         width: '100%',
         height: '100%',
-        resizeMode: 'contain',
     },
 });
+
+export const body_styles = StyleSheet.create({
+    bodyContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    Title: {
+        color: '#000000',
+        fontSize: Platform.OS === 'web' ? 48 : 32,
+        textAlign: 'center',
+        fontWeight: 'medium',
+        fontFamily: 'Manrope-Medium',
+    },
+    subTitle: {
+        color: '#4D5254',
+        fontSize: Platform.OS === 'web' ? 24: 16,
+        textAlign: 'center',
+        fontWeight: 'medium',
+        fontFamily: 'Manrope-Medium',
+    },
+})
+
+export const secure_styles = StyleSheet.create({
+    secureContainer: {
+        position: 'absolute',
+        top: Platform.OS === 'web' ? 50 : 75,
+        right: Platform.OS === 'web' ? 45 : 10,
+        width: Platform.OS === 'web' ? '18%' : '40%',
+        height: Platform.OS === 'web' ? '18%' : '10%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    secure: {
+        width: '100%',
+        height: '100%',
+    },
+})
 
 export const news_styles = StyleSheet.create({
     newsContainer: {
         width: '100%',
         height: '10%',
-        backgroundColor: '#252f3d',
-        justifyContent: 'center',
+        backgroundColor: '#252F3D',
+        justifyContent: 'space-between',
         alignItems: 'center',
         flexDirection: 'row',
         overflow: 'hidden',
       },
     newsText: {
-        marginLeft: 10,
         color: '#fff',
-        fontSize: 20,
-        fontFamily: 'Bond',
+        fontSize: 12,
+        fontWeight: 'medium',
+        fontFamily: 'Manrope-Medium',
+        marginLeft: 10,
     },
     newsBlock: {
-        marginLeft: 30,
+        width: Platform.OS === 'web' ? '25%' : '100%',
         justifyContent: 'center',
         alignItems: 'center',
         flexDirection: 'row',
         overflow: 'hidden',
+    },
+    newsImage: {
+        width: 24,
+        height: 24,
     },
 });
 
--- App/Styles/useCustomFonts.ts
+++ App/Styles/useCustomFonts.ts
@@ -1,9 +1,22 @@
 import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback } from 'react';
 
 export const useCustomFonts = () => {
-  const [fontsLoaded] = useFonts({
-    'Bond': require('../../assets/fonts/bond-bolditalic/Bond-BoldItalic.otf'),
+  const [fontsLoaded, fontError] = useFonts({
+    'Manrope-Medium': require('../../assets/fonts/Manrope/static/Manrope-Medium.ttf'),
+    'Manrope-Regular': require('../../assets/fonts/Manrope/static/Manrope-Regular.ttf'),
   });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
 
   return fontsLoaded;
 };
