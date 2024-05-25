13958703254fc6780d1856eed108b0b42d271092
ruteru <garcicon45@gmail.com>
2024-05-24 23:43:09 -0300

    update dependencies

--- metro.config.js
+++ metro.config.js
@@ -1,13 +1,15 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
-
 const config = {
   resolver: {
-    sourceExts: ['js', 'jsx', 'json', 'ts', 'tsx', 'cjs'],
-    assetExts: ['glb', 'gltf', 'mtl', 'obj', 'png', 'jpg', 'ttf', 'otf'],
+    sourceExts: ['js', 'jsx', 'json', 'ts', 'tsx', 'cjs', 'svg', 'svgx'],
+    assetExts: ['glb', 'gltf', 'mtl', 'obj', 'png', 'jpg', 'ttf', 'otf'].filter(ext => ext !== 'svg'),
   },
-}
+  transformer: {
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  },
+};
 
 config.resolver.assetExts.push("hcscript");
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config)
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
